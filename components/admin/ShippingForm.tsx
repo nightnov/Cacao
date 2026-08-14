2c31f1897d7df6684476887ad510ec03064704ad
nightnov <nightnova2007@gmail.com>
2026-08-14 18:19:29 +0000

    Fix: Ajouter @ts-ignore sur update tarifs livraison

--- components/admin/ShippingForm.tsx
+++ components/admin/ShippingForm.tsx
@@ -53,6 +53,7 @@ export default function ShippingForm({ fee, onClose }: ShippingFormProps) {
         // Update
         const { error: updateError } = await supabase
           .from('shipping_fees')
+          // @ts-ignore
           .update({
             city: formData.city,
             price_fcfa: formData.price_fcfa
